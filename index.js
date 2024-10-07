9a8cc7346827fe332e0be72dada52f5dc36d449b
Dean Maja <114931178+legendary2004@users.noreply.github.com>
2024-10-07 17:22:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import mysql from 'mysql2';
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import path from "path";
+import { fileURLToPath } from 'url';
 import ElasticEmail from "@elasticemail/elasticemail-client";
 import nodemailer from "nodemailer"
 import { profileUpload } from "./profileUpload.js"
@@ -12,12 +13,15 @@ dotenv.config({path: './.env'})
 
 const app = express();
 const port = 5000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
+
 
 
 const db = mysql.createConnection({
