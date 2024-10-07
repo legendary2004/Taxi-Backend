0aad2862efa607ea6f7734ed682c1fc48102a71f
Dean Maja <114931178+legendary2004@users.noreply.github.com>
2024-10-07 17:35:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -252,7 +252,7 @@ app.get('/message', (req, res) => {
     res.json({ message: "Hello from server!" });
 });
 
-app.get('*', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'index.html')); });
+app.get('*', (req, res) => { res.sendFile(path.join(__dirname, '../build', 'index.html')); });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
