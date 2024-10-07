714b886900194ac8b2642690909c694d7c900904
Dean Maja <114931178+legendary2004@users.noreply.github.com>
2024-10-07 17:14:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ dotenv.config({path: './.env'})
 const app = express();
 const port = 5000;
 
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
